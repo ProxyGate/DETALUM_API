7e0406fd542c559886a491729761281424f28d59
ProxyGate <83443746+ProxyGate@users.noreply.github.com>
2024-08-05 22:54:14 +0300

    Update test_catalog.py

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -1,8 +1,22 @@
 import time
+from abc import ABC, abstractmethod
+
 from progress.bar import IncrementalBar
 
 
-class TestCatalog:
+class BaseTestCatalog(ABC):
+
+    @abstractmethod
+    def test_tree(self, catalog):
+        pass
+
+    @abstractmethod
+    def test_parts(self, catalog):
+        pass
+
+
+class TestLemkenCatalog(BaseTestCatalog):
+
     def test_tree(self, catalog):
         response = catalog.get_tree()
 
@@ -10,186 +24,153 @@ class TestCatalog:
             data = response.json().get('data')
             if data:
                 bar = IncrementalBar(f'{catalog.name} categories', max=len(data), suffix='%(index)d/%(max)d ')
-                for category in data:
+                for category_data in data:
                     bar.next()
                     time.sleep(0.2)
-                    category_id = category.get('id')
-                    validation_fields = set()
-
-                    if catalog.name == 'lemken':
-                        validation_fields = {
-                            'id', 'name', 'parent_id', 'link_type', 'children',
-                            'created_at', 'updated_at', 'position', 'description',
-                            'remark', 'imageFields',
-                        }
-
-                        validation_image_fields = {'name', 's3'}
-                        image_fields = category.get('imageFields')
-
-                        if image_fields:
-                            image_missing_fields = validation_image_fields - image_fields.keys()
-
-                            if len(image_missing_fields) > 0:
-                                catalog.logger.warning(f'Missing fields  {image_missing_fields} in imageFields => in catalog: {catalog.name} category_id: {category_id}')
-                        else:
-                            catalog.logger.warning(f'No Image_fields in catalog: {catalog.name} category_id: {category_id}')
-                    elif catalog.name == 'grimme':
-                        validation_fields = {
-                            'id', 'label', 'parent_id', 'linkType',
-                            'children', 'created_at', 'updated_at',
-                        }
-                    elif catalog.name == 'claas':
-                        validation_fields = {
-                            'id', 'name', 'parent_id', 'link_type',
-                            'children', 'created_at', 'updated_at',
-                            'depth',
-                        }
-
-                    missing_fields = validation_fields - category.keys()
-
-                    if len(missing_fields) > 0:
-                        catalog.logger.warning(f'Missing fields {missing_fields} in catalog: {catalog.name} category_id: {category_id}')
+                    category_id = category_data.get('id')
 
                     catalog.add_category(category_id=category_id)
+                    catalog.categories[category_id].validate(data=category_data)
+
                 bar.finish()
             else:
                 catalog.logger.warning(f'No data in {catalog.name} {catalog.current_url}')
         else:
             catalog.logger.warning(f'Bad request {catalog.name} {catalog.current_url}')
 
-        if catalog.categories:
-            main_parts_list_ids = []
+        subcategories = dict()
 
-            bar = IncrementalBar(f'receive categories data from {catalog.name} ', max=len(catalog.categories[-2:-1]), suffix='%(index)d/%(max)d ')
-            for category in catalog.categories[-2:-1]:
+        if catalog.categories:
+            bar = IncrementalBar(f'receive categories data from {catalog.name} ', max=len(list(catalog.categories)[-2:]), suffix='%(index)d/%(max)d ')
+            for category_id in list(catalog.categories)[-2:]:
                 bar.next()
                 time.sleep(0.1)
-                response = catalog.get_category(category_or_children_id=category.id)
+                response = catalog.get_category(category_or_children_id=category_id)
 
                 if response.status_code != 200:
-                    catalog.logger.warning(f'Bad request catalog: {catalog.name} category_id: {category.id} {catalog.current_url}')
+                    catalog.logger.warning(f'Bad request catalog: {catalog.name} category_id: {category_id} {catalog.current_url}')
                     continue
 
                 data = response.json().get('data')
 
                 if not data:
-                    catalog.logger.warning(f'Note data in catalog: {catalog.name} category_id: {category.id}')
+                    catalog.logger.warning(f'Note data in catalog: {catalog.name} category_id: {category_id}')
                     continue
 
-                for el in data:
-                    children = el.get('children')
+                for subcategory in data:
+                    children = subcategory.get('children')
 
                     if not children:
-                        catalog.logger.warning(f'No children in data catalog: {catalog.name} category_id: {category.id}')
+                        catalog.logger.warning(f'No children in data catalog: {catalog.name} category_id: {category_id}')
                         continue
 
-                    for child in children:
+                    for index, child in enumerate(children):
                         child_id = child.get('id')
-                        main_parts_list_ids.append(child_id)
+                        subcategories[f"{index}_-_{category_id}"] = child_id
             bar.finish()
 
-            children_parts_list_ids = []
+            parts_list = dict()
 
-            bar = IncrementalBar(f'receive parts list', max=len(main_parts_list_ids), suffix='%(index)d/%(max)d ')
-            for part_list_id in main_parts_list_ids:
+            bar = IncrementalBar(f'receive parts list', max=len(subcategories), suffix='%(index)d/%(max)d ')
+            for category_data, subcategory_id in subcategories.items():
+                category_id = category_data.split('_-_')[-1]
                 bar.next()
-                response = catalog.get_category(category_or_children_id=part_list_id)
+                response = catalog.get_category(category_or_children_id=subcategory_id)
 
                 if response.status_code != 200:
-                    catalog.logger.warning(f'Bad request catalog: {catalog.name} part_list_id: {part_list_id} {catalog.current_url}')
+                    catalog.logger.warning(
+                        f'Bad request catalog: {catalog.name} category_id: {category_id} part_list_id: {subcategory_id} {catalog.current_url}')
                     continue
 
                 data = response.json().get('data')
 
                 if not data:
-                    catalog.logger.warning(f'No data in catalog: {catalog.name} main_part_list_id: {part_list_id}')
+                    catalog.logger.warning(f'No data in catalog: {catalog.name} category_id: {category_id} subcategory_id: {subcategory_id}')
                     continue
 
                 for el in data:
                     children = el.get('children')
 
                     if not children:
-                        catalog.logger.warning(f'No children in data catalog: {catalog.name} main_part_list_id: {part_list_id}')
+                        catalog.logger.warning(
+                            f'No children in data catalog: {catalog.name} category_id: {category_id} subcategory_id: {subcategory_id}')
                         continue
 
-                    for child in children:
+                    for index, child in enumerate(children):
                         child_id = child.get('id')
-                        children_parts_list_ids.append(child_id)
+                        parts_list[f"{index}_-_{subcategory_id}_-_{category_id}"] = child_id
             bar.finish()
 
-            bar = IncrementalBar(f'receive parts from parts list', max=len(children_parts_list_ids), suffix='%(index)d/%(max)d ')
-            for part_list_id in children_parts_list_ids:
+            bar = IncrementalBar(f'receive parts from parts list', max=len(parts_list), suffix='%(index)d/%(max)d ')
+            for category_data, part_list_id in parts_list.items():
+                _, subcategory_id, category_id = category_data.split('_-_')
                 bar.next()
                 response = catalog.get_parts(child_id=part_list_id)
 
                 if response.status_code != 200:
-                    catalog.logger.warning(f'Bad request catalog: {catalog.name} part_list_id: {part_list_id} {catalog.current_url}')
+                    catalog.logger.warning(
+                        f'Bad request catalog: {catalog.name} category_id: {category_id} subcategory_id: {subcategory_id} part_list_id: {part_list_id} {catalog.current_url}')
                     continue
 
                 data = response.json().get('data')
 
                 if not data:
-                    catalog.logger.warning(f'No Parts in {catalog.name} part_list_id: {part_list_id}')
+                    catalog.logger.warning(f'No Parts in {catalog.name} category_id: {category_id} subcategory_id: {subcategory_id} part_list_id: {part_list_id}')
                     continue
 
                 for part in data:
                     part_id = part.get('id')
-                    catalog.parts.append(part_id)
-
+                    category = catalog.categories[int(category_id)]
+                    category.add_part(part_id=part_id)
             bar.finish()
         else:
             catalog.logger.warning(f'No Categories in {catalog.name}')
 
     def test_parts(self, catalog):
-        if catalog.parts:
-            bar = IncrementalBar(f'check fields in detail', max=len(catalog.parts), suffix='%(index)d/%(max)d ')
-            for part_id in catalog.parts:
+        parts = []
+
+        for el in catalog.categories.values():
+            parts.extend(el.parts)
+
+        if parts:
+            bar = IncrementalBar(f'check fields in detail', max=len(parts), suffix='%(index)d/%(max)d ')
+            for part in parts:
                 bar.next()
-                response = catalog.get_part(part_id=part_id)
+                response = catalog.get_part(part_id=part.id)
 
                 if response.status_code != 200:
-                    catalog.logger.warning(f'Bad request catalog: {catalog.name} part_id: {part_id} {catalog.current_url}')
+                    catalog.logger.warning(
+                        f'Bad request catalog: {catalog.name} part_id: {part.id} {catalog.current_url}')
                     continue
 
                 data = response.json().get('data')
 
                 if not data:
-                    catalog.logger.warning(f'No data in {catalog.name} Part_id: {part_id})')
+                    catalog.logger.warning(f'No data in {catalog.name} Part_id: {part.id})')
                     continue
 
-                validation_fields = {
-                    'id', 'name', 'link_type', 'quantity',
-                    'part_number', 'position', 'dimension',
-                    'imageFields', 'created_at', 'updated_at',
-                }
-
-                missing_fields = validation_fields - data.keys()
-
-                if len(missing_fields) > 0:
-                    catalog.logger.warning(f'Missing fields {missing_fields} in {catalog.name} part_id: {part_id}')
-
-                image_fields = data.get('imageFields')
-
-                if image_fields:
-                    validation_image_fields = {'name', 's3'}
-
-                    image_missing_fields = validation_image_fields - image_fields.keys()
-
-                    if len(image_missing_fields) > 0:
-                        catalog.logger.warning(f'Missing fields  {image_missing_fields} in imageFields => in {catalog.name} part_id: {part_id}')
-                else:
-                    catalog.logger.warning(f'Not Image_fields in {catalog.name} part_id: {part_id}')
-
-                part_category = response.json().get('category')
-
-                if part_category:
-                    validation_fields = {'id'}
-                    missing_fields = validation_fields - part_category.keys()
-
-                    if len(missing_fields) > 0:
-                        catalog.logger.warning(f'Missing fields {missing_fields} in {catalog.name} part_id: {part_id}')
-                else:
-                    catalog.logger.warning(f'No part_category in {catalog.name} part_id: {part_id}')
+                part.validate(data=data)
             bar.finish()
         else:
             catalog.logger.warning(f'No parts in catalog: {catalog.name}')
+
+
+class TestCatalog:
+    """class wrap for start test"""
+    def _get_test_instance(self, catalog):
+        catalog_name = catalog.name.capitalize()
+        test_class_name = f"Test{catalog_name}Catalog"
+        test_class = globals().get(test_class_name)
+
+        if test_class is None:
+            raise ValueError(f"Test class {test_class_name} is not defined.")
+
+        return test_class()
+
+    def test_tree(self, catalog):
+        instance = self._get_test_instance(catalog)
+        instance.test_tree(catalog)
+
+    def test_parts(self, catalog):
+        instance = self._get_test_instance(catalog)
+        instance.test_parts(catalog)
