22cfef602bc762ebebea07f8341c5a609e899276
ProxyGate <83443746+ProxyGate@users.noreply.github.com>
2024-10-14 15:50:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-import threading
-import time
+import asyncio
+import aiohttp
 import pytest
-import requests
 from InquirerPy import inquirer
-from progress.spinner import Spinner
 from colorama import Fore, init
+from tqdm import tqdm
+from utility import update_spinner, get_ip_address
+from database import initialize_db
 
 # Инициализация colorama
 init(autoreset=True)
-
-
-def update_spinner(spin, spin_event):
-    while not spin_event.is_set():
-        spin.next()
-        time.sleep(0.1)
 
 
 class Level:
@@ -46,53 +41,60 @@ class Level:
 level = Level()
 
 
-def start_app():
-    print(Fore.GREEN + 'App for testing API https://detalum.ru/')
-    choice = inquirer.select(
-        message="Choice continue or exit:",
-        qmark='',
-        amark='',
-        choices=['continue', 'exit'],
-    ).execute()
+async def fetch_brands(session):
+    url = 'http://api.catalog.detalum.ru/api/v1/brand'
 
-    if choice == 'continue':
-        spinner = Spinner(Fore.CYAN + 'Loading Brands ')
-        stop_spinner = threading.Event()
-
-        thread = threading.Thread(target=update_spinner, args=(spinner, stop_spinner))
-        thread.start()
-
-        url = 'http://api.catalog.detalum.ru/api/v1/brand'
-        response = requests.get(url)
-
-        brands = dict()
-
-        if response.status_code == 200:
-            data = response.json().get('data')
-            if data:
-                brands = {brand.get('label'): brand.get('slug') for brand in data}
-            else:
-                print(Fore.RED + f'No data in responuse {url}')
+    async with session.get(url) as response:
+        if response.status == 200:
+            data = await response.json()
+            return {brand.get('label'): brand.get('slug') for brand in data.get('data', [])}
         else:
             print(Fore.RED + f'Bad request {url}')
+            return None
 
-        stop_spinner.set()
-        thread.join()
 
-        if brands:
-            level + 1
-            brands['Тест API для всех каталогов'] = f"-s -v tests/test_catalog.py::TestCatalog::test_root_categories tests/test_catalog.py::TestCatalog::test_tree tests/test_catalog.py::TestCatalog::test_parts --catalogs={','.join(brands.values())} --test_api"
-            level.add_menu(brands)
-            open_menu()
-        else:
-            print(Fore.YELLOW + '\n No Brands.')
-            start_app()
+async def start_app():
+    try:
+        print(Fore.GREEN + 'App for testing API https://detalum.ru/')
 
-    elif choice == 'exit':
+        choice = await inquirer.select(
+            message="Choice continue or exit:",
+            qmark='',
+            amark='',
+            choices=['continue', 'exit'],
+        ).execute_async()
+
+        if choice == 'continue':
+            spinner = tqdm(total=0, bar_format="{desc}", ncols=30)
+            spinner_text = Fore.CYAN + 'Loading Brands'
+            spinner_event = asyncio.Event()
+            spinner_task = asyncio.create_task(update_spinner(spin=spinner, spin_text=spinner_text, spin_event=spinner_event))
+            try:
+                async with aiohttp.ClientSession() as session:
+                    brands = await fetch_brands(session)
+            finally:
+                spinner_event.set()
+                await spinner_task
+                spinner.set_description('Brands loaded')
+                spinner.close()
+
+            if brands:
+                level + 1
+                brands['Тест API для всех каталогов'] = f"-s -v tests/test_catalog.py::TestCatalog::test_root_categories tests/test_catalog.py::TestCatalog::test_tree tests/test_catalog.py::TestCatalog::test_parts --catalogs={','.join(brands.values())} --test_api --alluredir allure_results"
+                ip_address = await get_ip_address()
+                brands['Запустить Allure'] = f"allure serve --host {ip_address} --port 8080 allure_results"
+                level.add_menu(brands)
+                await open_menu()
+            else:
+                print(Fore.YELLOW + '\n No Brands.')
+                await start_app()
+        elif choice == 'exit':
+            exit()
+    except KeyboardInterrupt:
         exit()
 
 
-def open_menu(**kwargs):
+async def open_menu(**kwargs):
     menu = level.get_menu()
 
     if isinstance(menu, tuple):
@@ -105,37 +107,63 @@ def open_menu(**kwargs):
         brand = kwargs.get('brand')
         message = f"Brand {brand}:"
 
-    choice = inquirer.select(
+    choice = await inquirer.select(
         message=message,
         choices=menu_list,
         qmark='',
         amark='',
         height=len(menu_list),
-    ).execute()
+    ).execute_async()
 
     if choice.lower()[:4] == 'тест':
         command = menu.get(choice).split()
         pytest.main(command)
-        open_menu()
+        await open_menu()
+    elif choice == 'Запустить Allure':
+        command = menu.get(choice)
+        try:
+            process = await asyncio.create_subprocess_shell(command)
+            await process.wait()
+        except asyncio.CancelledError:
+            print("Allure был завершён. Возврат в меню.")
+            await open_menu()
+        except KeyboardInterrupt:
+            print("Процесс был прерван. Возврат в меню.")
+        except Exception as e:
+            print(f"Произошла ошибка при запуске Allure: {e}")
+        finally:
+            await open_menu()
     elif choice == '<<< назад >>>':
         level - 1
         if level > 0:
-            open_menu()
+            await open_menu()
         else:
             level.clean()
-            start_app()
+            await start_app()
     else:
         brand_slug = menu.get(choice)
         catalog_menu = {
-                'Тест каталога': f'-s -v tests/test_catalog.py::TestCatalog::test_root_categories tests/test_catalog.py::TestCatalog::test_tree tests/test_catalog.py::TestCatalog::test_parts --catalogs={brand_slug}',
-                'Тест дерева': f'-s -v tests/test_catalog.py::TestCatalog::test_root_categories tests/test_catalog.py::TestCatalog::test_tree --catalogs={brand_slug}',
-                'Тест корневых категорий': f'-s -v tests/test_catalog.py::TestCatalog::test_root_categories --catalogs={brand_slug}',
-                'Тест API': f'-s -v tests/test_catalog.py::TestCatalog::test_root_categories tests/test_catalog.py::TestCatalog::test_tree tests/test_catalog.py::TestCatalog::test_parts --catalogs={brand_slug} --test_api',
+                'Тест каталога': f'-s -v tests/test_catalog.py::TestCatalog::test_root_categories tests/test_catalog.py::TestCatalog::test_tree tests/test_catalog.py::TestCatalog::test_parts --catalogs={brand_slug} --alluredir allure_results',
+                'Тест дерева': f'-s -v tests/test_catalog.py::TestCatalog::test_root_categories tests/test_catalog.py::TestCatalog::test_tree --catalogs={brand_slug} --alluredir allure_results',
+                'Тест корневых категорий': f'-s -v tests/test_catalog.py::TestCatalog::test_root_categories --catalogs={brand_slug} --alluredir allure_results',
+                'Тест API': f'-s -v tests/test_catalog.py::TestCatalog::test_root_categories tests/test_catalog.py::TestCatalog::test_tree tests/test_catalog.py::TestCatalog::test_parts --catalogs={brand_slug} --test_api --alluredir allure_results',
         },
         level + 1
         level.add_menu(catalog_menu)
-        open_menu(brand=choice)
+        await open_menu(brand=choice)
 
+
+async def main():
+    await initialize_db()
+    await start_app()
 
 if __name__ == '__main__':
-    start_app()
+    try:
+        asyncio.run(main())
+    except asyncio.CancelledError:
+        exit()
+    except KeyboardInterrupt:
+        exit()
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+        exit()
